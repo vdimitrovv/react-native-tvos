6c611f59697f9ab8b4afbe2e8fd320b475435019
Ventsislav Dimitrov <4097884+vdmtrv@users.noreply.github.com>
2020-05-04 11:32:11 +0100

    fix: native dpad toggle to use JS focus engine

--- ReactAndroid/src/main/java/com/facebook/react/uimanager/UIManagerModule.java
+++ ReactAndroid/src/main/java/com/facebook/react/uimanager/UIManagerModule.java
@@ -111,6 +111,7 @@ public class UIManagerModule extends ReactContextBaseJavaModule
 
   private static final boolean DEBUG =
       PrinterHolder.getPrinter().shouldDisplayLogMessage(ReactDebugOverlayTags.UI_MANAGER);
+  private static boolean sIsNativeDpadEnabled = true;
 
   private final EventDispatcher mEventDispatcher;
   private final Map<String, Object> mModuleConstants;
@@ -168,6 +169,15 @@ public class UIManagerModule extends ReactContextBaseJavaModule
             minTimeLeftInFrameForNonBatchedOperationMs);
 
     reactContext.addLifecycleEventListener(this);
+  }
+
+  public static boolean isNativeDpadEnabled() {
+    return sIsNativeDpadEnabled;
+  }
+
+  @ReactMethod
+  public void enableNativeDpad(boolean enable) {
+    sIsNativeDpadEnabled = enable;
   }
 
   @Deprecated
